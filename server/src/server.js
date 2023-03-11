139380c78635689af11cea6224308d348fc24e44
Ehab-24 <109919400+Ehab-24@users.noreply.github.com>
2023-03-11 20:38:24 +0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    `mongodb+srv://Ehab-24:${process.env.mongoPassword}@cluster1.uo8fklj.mongodb.net/?retryWrites=true/myshop`,
+    `mongodb://localhost/myShop`,
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
   )
   .then(() => console.log("Connected to MongoDB..."))
